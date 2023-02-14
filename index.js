bff4ebc45cf8742ce95df0fcab921635b69a7b45
Matthew Clark <afromatt6288@hotmail.com>
2023-02-14 11:21:39 -0700

    created the like and dislike event listeners, and patched them to db

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ fetch("http://localhost:3000/florida_man")
 .then(fmData => {
         grabTopTen(fmData)
         grabByDate(fmData)
-        // renderTopTen()
-        // renderFmDetails()
-        // topTenClick()
-})
-
+        // renderTopTen() // called within other functions
+        // renderFmDetails() // called within other functions
+        // topTenClick() // called within other functions
+    })
+        
 function grabTopTen(fmData){
     fmData
     .sort((a, b) => b.likes - a.likes)
@@ -63,8 +63,8 @@ function renderFmDetails(fm) {
     let fmImage = document.querySelector('#image')
     let fmArticle = document.querySelector('#article')
     let fmArticleLink = document.querySelector('#link')
-    let fmLike = document.querySelector('#like')
-    let fmDislike = document.querySelector('#dislike')
+    let fmLike = document.querySelector('.like')
+    let fmDislike = document.querySelector('.dislike')
     
     fmDate.textContent = currentFM.date
     fmTitle2.textContent = currentFM.title
@@ -73,6 +73,46 @@ function renderFmDetails(fm) {
     fmArticleLink.href = currentFM.Article_link
     fmLike.textContent = `${currentFM.likes} likes`
     fmDislike.textContent = `${currentFM.dislikes} dislikes`
+    
+    fmLike.id = currentFM.id
+    fmLike.addEventListener('click', (e) => {
+        let id = e.target.id
+
+        currentFM.likes++
+        let newLikes = currentFM.likes
+        fmLike.textContent = `${newLikes} likes`
+        
+        fetch(`http://localhost:3000/florida_man/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                "likes": newLikes
+            }) 
+        })
+    })
+
+    fmDislike.id = currentFM.id
+    fmDislike.addEventListener('click', (e) => {
+        let id = e.target.id
+
+        currentFM.dislikes++
+        let newDislikes = currentFM.dislikes
+        fmDislike.textContent = `${newDislikes} dislikes`
+        
+        fetch(`http://localhost:3000/florida_man/${id}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                "dislikes": newDislikes
+            }) 
+        })
+    })
 }
 
 
